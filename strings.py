3b45c16ebba553141465cfb5ab1129de9c030c37
kara0412 <kw412@live.com>
2020-03-31 15:38:53 -0500

    Adjust wording for general rate limit messaging

--- strings.py
+++ strings.py
@@ -11,7 +11,7 @@ RULES = "Rules? For a bot? Well, this is a spanking bot after all. And you know 
         "\n2. You can only add %s swats at a time." \
         "\n3. You can only subtract %s swats at a time." \
         "\n4. You must wait %s minutes between sending swats to any particular person." \
-        "\n5. You can only send %s distinct people swats in a %s minute window."\
+        "\n5. You can only send %s swat adjustments in a %s minute window."\
         "\n\nEach infraction will swiftly earn you 5 additional swats. If you " \
         "need a refresher, just type /rules anytime to see this list." % \
         (MAX_INC, MAX_DEC, PER_PERSON_TIME_LIMIT, TIME_WINDOW_LIMIT_COUNT, TIME_WINDOW)
@@ -24,7 +24,7 @@ PENALTY_SCOLDS = {
                         "a %s minute cool-down period before you can swat them again." %
                         ('%s', PER_PERSON_TIME_LIMIT),
     "LIMIT_PER_TIME_WINDOW": "Let's not get carried away... you can only send %s "
-                             "distinct people swats within a %s minute window." %
+                             "swat adjustments within a %s minute window." %
                              (TIME_WINDOW_LIMIT_COUNT, TIME_WINDOW)
 }
 
