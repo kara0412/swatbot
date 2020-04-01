ff19ad7224eff9ab33e8016b5383e94b81e6ffce
kara0412 <kw412@live.com>
2020-04-01 01:11:11 -0500

    Add milestones and adjust some message wording

--- strings.py
+++ strings.py
@@ -10,10 +10,10 @@ RULES = "Rules? For a bot? Well, this is a spanking bot after all. And you know 
         "\n2. You can only add %s swats at a time." \
         "\n3. You can only subtract %s swats at a time." \
         "\n4. You must wait %s minutes between sending swats to any particular person." \
-        "\n5. You can only send %s swat adjustments in a %s minute window."\
+        "\n5. You can only add or subtract swats %s times in a %s minute window."\
         "\n\nEach infraction will swiftly earn you 5 additional swats. These rules " \
-        "are neither comprehensive nor invariable. If I need to add more, I certainly " \
-        "will. If you need a refresher, just type /rules anytime to see this list." % \
+        "are neither comprehensive nor invariable. More rules could be added at any " \
+        "point. If you need a refresher, just type /rules anytime to see this list." % \
         (env_vars["MAX_INC"], env_vars["MAX_DEC"], env_vars["PER_PERSON_TIME_LIMIT"], env_vars["TIME_WINDOW_LIMIT_COUNT"], env_vars["TIME_WINDOW"])
 
 PENALTY_SCOLDS = {
@@ -21,17 +21,22 @@ PENALTY_SCOLDS = {
     "OWN_SWAT": "Trying to subtract your own swats? Nice try.",
     "SWAT_INC": "Remember, the rules say you can only add %s swats at a time." % env_vars["MAX_INC"],
     "SWAT_DEC": "Remember, the rules say you can only decrease %s swats at time." % env_vars["MAX_DEC"],
-    "LIMIT_PER_PERSON": "Whoa there... you just adjusted the swat count for %s! Remember, there's "
-                        "a %s minute cool-down period before you can swat them again." %
-                        ('%s', env_vars["PER_PERSON_TIME_LIMIT"]),
-    "LIMIT_PER_TIME_WINDOW": "Let's not get carried away... you can only send %s "
-                             "swat adjustments within a %s minute window." %
+    "LIMIT_PER_PERSON": "Whoa there... remember, you have to wait %s minutes before you can "
+                        "add or subtract swats for %s again!" %
+                        (env_vars["PER_PERSON_TIME_LIMIT"], '%s'),
+    "LIMIT_PER_TIME_WINDOW": "Let's not get carried away... you can only add or "
+                             "subtract swats %s times within a %s minute window." %
                              (env_vars["TIME_WINDOW_LIMIT_COUNT"], env_vars["TIME_WINDOW"])
 }
 
 ERROR_MSG = "Oops! Something went wrong, and I'm now broken." # catch-all if swatbot starts malfunctioning
 
 messages = OrderedDict()
-messages[50] = "Over 50, ouch! I do not envy you right now."
+messages[50] = "You reached 50, ouch! I do not envy you right now."
 messages[100] = "Oh boy, over 100 swats! Looks like you have a big spanking coming..."
+messages[150] = "I'd start behaving if I were you."
+messages[200] = "You may never sit again..."
+messages[250] = "This is getting out of hand."
+messages[300] = "You're going to regret this, trust me."
+
 MILESTONES = messages
