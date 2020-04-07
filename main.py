ba92730ae3d35e5c1b93dc990cd48e7138e09ae9
kara0412 <kw412@live.com>
2020-04-06 22:13:41 -0500

    Add Ami text

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telegram import MessageEntity
 
 from settings import env_vars
 from strings import SWAT_UPDATE_STRING, RULES, PENALTY_SCOLDS, MILESTONES, \
-    ERROR_MSG, MY_SWATS, SWAT_COUNT_NO_MENTION, SWAT_COUNT, CONVERSION
+    ERROR_MSG, MY_SWATS, SWAT_COUNT_NO_MENTION, SWAT_COUNT, CONVERSION, AMI_TEXT
 from db_helpers import update_user_count_in_db, get_user_count_from_db, \
     get_nth_recent_swat_time, update_history_in_db
 import sentry_sdk
@@ -83,6 +83,10 @@ def swat_count(update, context):
     else:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=SWAT_COUNT_NO_MENTION)
+
+def ami(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=AMI_TEXT)
+
 
 def conversion(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=CONVERSION)
@@ -207,9 +211,11 @@ def main():
     my_swats_handler = CommandHandler('my_swats', my_swats)
     swat_count_handler = CommandHandler('swat_count', swat_count)
     conversion_handler = CommandHandler('conversions', conversion)
+    ami_handler = CommandHandler('override_Ami_code5778', ami)
     mention_handler = MessageHandler(swatExistsFilter, mention_response)
     add_handlers_to_dispatcher([start_handler, rules_handler, my_swats_handler,
-                                swat_count_handler, conversion_handler, mention_handler])
+                                swat_count_handler, conversion_handler,
+                                ami_handler, mention_handler])
     updater.start_webhook(listen='0.0.0.0', port=env_vars["PORT"], url_path=env_vars["TOKEN"])
     updater.bot.set_webhook(env_vars["WEBHOOK_URL"] + env_vars["TOKEN"])
     updater.idle()
